a7800ad1be01d1845cb786c3fd38c6b1dce53d75
ZenithSu <happy@hakcing.icu>
2024-05-11 23:17:47 +0800

    Add read method to IPersistence and logging in StateMachineServiceProvider
    A read method was added to the IPersistence interface to read the current state of an event. Additionally, detailed logging was implemented in the StateMachineServiceProvider, both for handling state transitions as well as firing associated actions. This provides better visibility on the state changes and actions trigger for debugging and analysis purposes.

--- src/IPersistence.php
+++ src/IPersistence.php
@@ -7,6 +7,7 @@ namespace Zenith\LaravelStateMachine;
 interface IPersistence
 {
 
+    public function read($event): string;
 
     public function write($event, string $source, string $target);
 
--- src/StateMachineServiceProvider.php
+++ src/StateMachineServiceProvider.php
@@ -6,6 +6,7 @@ namespace Zenith\LaravelStateMachine;
 
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class StateMachineServiceProvider extends ServiceProvider
@@ -30,11 +31,26 @@ class StateMachineServiceProvider extends ServiceProvider
             $sm = $this->app->make($stateMachine);
             foreach ($sm->getConfigurer()->getTransitions() as $transition) {
                 Event::listen($transition->getEvent(), function ($event) use ($sm, $transition) {
+                    $currentStatus = $sm->getConfigurer()->getPersistence()->read($event);
+                    if ($currentStatus !== $transition->getSource()) {
+                        Log::warning('State does not match expected source', [
+                            'event' => $transition->getEvent(),
+                            'expected' => $transition->getSource(),
+                            'actual' => $currentStatus,
+                        ]);
+                        return ;
+                    }
                     $sm->getConfigurer()->getPersistence()->write($event, $transition->getSource(), $transition->getTarget());
+                    Log::info('Transition happened', [
+                        'event' => get_class($event),
+                        'source' => $transition->getSource(),
+                        'target' => $transition->getTarget(),
+                    ]);
                 });
                 $actions = $transition->getActions();
                 foreach ($actions as $action) {
                     Event::listen($transition->getEvent(), $action);
+                    Log::info('Action fired', ['event' => $transition->getEvent(), 'action' => get_class($action)]);
                 }
             }
         }
